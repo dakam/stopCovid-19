71c53b472193f33d0ad4b7447878ced0d29fcf77
dakam <katodakam2010@gmail.com>
2020-04-10 09:34:45 +0300

    Node js basics

---
+++ basics.js
@@ -0,0 +1,25 @@
+var num=5;
+var res = num*20;
+
+console.log(res);
+
+for(var i=0;i<=6;i++)
+{
+   
+
+    if(i==4)
+    {
+     
+        continue;
+    }
+
+    console.log(i);
+
+}
+var k=0;
+while(k<5)
+{
+    console.log("test");
+    k++;
+
+}
